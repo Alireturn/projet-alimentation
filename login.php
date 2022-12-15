abf3bb27d1b2c6ec5bb5c10982d8891150e3bf18
unknown <alib.38@outlook.fr>
2022-12-15 16:15:59 +0100

    includes header and footer

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php 
+$page=[
+    "title" => "Track Calorie - Login"
+];
+
+
+include_once("includes/header.php");
+
+?>
+
+<h1>Login Page</h1>
+    
+
+
+
+
+<?php
+include_once('includes/header.php');
+
+?>
